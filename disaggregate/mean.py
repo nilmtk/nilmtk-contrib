ac18e7ace3bd73c722f70f02f15392abc6f5e9b5
ayushp20.com <ayush.p.20@gmail>
2019-06-11 17:51:02 +0000

    Cleaned up mean.py
    Closes #1

--- disaggregate/mean.py
+++ disaggregate/mean.py
@@ -3,12 +3,8 @@ from warnings import warn
 
 import pandas as pd
 import numpy as np
-import pickle
-import copy
 
-from nilmtk.utils import find_nearest
 from nilmtk.disaggregate import Disaggregator
-from nilmtk.datastore import HDFDataStore
 
 # Fix the seed for repeatability of experiments
 SEED = 42
@@ -22,93 +18,44 @@ class Mean(Disaggregator):
     ----------
     model : list of dicts
        Each dict has these keys:
-           states : list of ints (the power (Watts) used in different states)
-           training_metadata : ElecMeter or MeterGroup object used for training
-               this set of states.  We need this information because we
-               need the appliance type (and perhaps some other metadata)
-               for each model.
-
-    state_combinations : 2D array
-        Each column is an appliance.
-        Each row is a possible combination of power demand values e.g.
-            [[40],
-             [100],
-             [50],
-             [60], ...]
+           mean : list of mean values, one for each appliance (the mean power
+           (Watts)) training_metadata : The appliance type (and perhaps some
+           other metadata) for each model.
 
     MIN_CHUNK_LENGTH : int
+
+    MODEL_NAME = string
     """
 
     def __init__(self):
         self.model = []
-        self.state_combinations = None
         self.MIN_CHUNK_LENGTH = 100
         self.MODEL_NAME = 'Mean'
 
-
-    
     def partial_fit(self, train_main, train_appliances, **load_kwargs):
-        
-        print("...............Function in partial_fit mean  .............\n\n")
-        
-        num_on_states=None
-        if len(train_appliances)>12:
-            max_num_clusters=2
-        else:
-            max_num_clusters=3
-        appliance_in_model=[d['training_metadata'] for d in self.model]
 
-        for appliance,readings in train_appliances:
- 
-            if appliance in appliance_in_model:
-                raise RuntimeError(
-                "Appliance {} is already in model!"
-                "  Can't train twice on the same meter!",appliance)
+        print("...............Mean partial_fit running...............")
 
-            states=readings.mean(skipna=True)
-            states = np.round(states).astype(np.int32)
+        for appliance_name, power in train_appliances:
+            mean = np.nanmean(power)
+            mean = np.round(mean).astype(np.int32)
+
             self.model.append({
-                    'states': states,
-                    'training_metadata': appliance})
+                    'mean': mean,
+                    'training_metadata': appliance_name})
 
-    def _set_state_combinations_if_necessary(self):
-        """Get centroids"""
-        # If we import sklearn at the top of the file then auto doc fails.
-        if (self.state_combinations is None or
-                self.state_combinations.shape[1] != len(self.model)):
+    def disaggregate_chunk(self, test_mains):
 
-            from sklearn.utils.extmath import cartesian
-            mean = [model['states'] for model in self.model]    
-            self.state_combinations = cartesian(mean)
+        print("...............Mean disaggregate_chunk running...............")
 
-    def disaggregate_chunk(self, mains):
-        
-        if len(mains) < self.MIN_CHUNK_LENGTH:
+        if len(test_mains) < self.MIN_CHUNK_LENGTH:
             raise RuntimeError("Chunk is too short.")
-
-        # sklearn produces lots of DepreciationWarnings with PyTables
-        import warnings
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-
-        self._set_state_combinations_if_necessary()
-
-
-        state_combinations = self.state_combinations
-
-        summed_power_of_each_combination = np.sum(state_combinations, axis=1)
-
-        # Start disaggregation
-        indices_of_state_combinations, residual_power = find_nearest(
-            summed_power_of_each_combination, mains.values)
 
         appliance_powers_dict = {}
         for i, model in enumerate(self.model):
-            print("Estimating power demand for '{}'"
-                  .format(model['training_metadata']))
-            predicted_power = state_combinations[
-                indices_of_state_combinations, i].flatten()
-            column = pd.Series(predicted_power, index=mains.index, name=i)
+            predicted_power = [self.model[i]['mean'] for j in range(0, test_mains.shape[0])]
+            column = pd.Series(predicted_power, index=test_mains.index, name=i)
             appliance_powers_dict[self.model[i]['training_metadata']] = column
+
         appliance_powers = pd.DataFrame(appliance_powers_dict, dtype='float32')
         return appliance_powers
