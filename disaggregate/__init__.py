1d73a4c55b5809804c8b64137402c1c0b014b313
ayushp20.com <ayush.p.20@gmail>
2019-06-11 18:49:10 +0000

    removed unimplemented algorithms from __init__.py

--- disaggregate/__init__.py
+++ disaggregate/__init__.py
@@ -1,7 +1,5 @@
 from .disaggregator import Disaggregator
 from .combinatorial_optimisation import CombinatorialOptimisation
 from .fhmm_exact import FHMM
-from .hart_85 import Hart85
-from .maximum_likelihood_estimation import MLE
 from .mean import Mean
 from .zero import Zero
