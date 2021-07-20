3ce66a664bf40e1ed0acb2c0e5a21419b17adede
Hetvi Shastri <hetvishastri99@gmail.com>
2021-07-21 04:54:41 +0530

    modified init.py for new networks

--- nilmtk_contrib/disaggregate/__init__.py
+++ nilmtk_contrib/disaggregate/__init__.py
@@ -7,3 +7,8 @@ from .seq2point import Seq2Point
 from .seq2seq import Seq2Seq
 from .WindowGRU import WindowGRU
 from .rnn import RNN
+from .rnn_attention import RNN_attention
+from .rnn_attention_classification import RNN_attention_classification
+from .resnet import ResNet
+from .resnet_classification import ResNet_classification
+from .bert import BERT
